98f4a29290aa70464d6c5cf7fd2258a9701e27e8
Muhammad-Huzaifa-Ayub <m.huzaifa.ayub.510@gmail.com>
2026-09-05 07:37:11 +0500

    solved

---
+++ 258.cpp
@@ -0,0 +1,51 @@
+/*
+258.add digits
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2
+Since 2 has only one digit, return it.
+Example 2:
+
+Input: num = 0
+Output: 0
+
+Constraints:
+
+0 <= num <= 2^31 - 1
+
+Follow up: Could you do it without any loop/recursion in O(1) runtime?
+
+*/
+
+#include<iostream>
+
+int addDigits(int num)
+{
+
+    while ( num >= 10 )
+    {
+        int sum = 0;
+        while ( num > 0 )
+        {
+        	sum += num % 10;
+        	num = num / 10;
+				        		
+		}
+		num = sum;	
+    }
+    return num;
+}
+
+int main()
+{
+	int num = 423514166;
+	std::cout << addDigits(num);
+
+    return 0;
+}
+	

f3f38f7282a6c2cad73737ad3540555e28de8bbd
Muhammad-Huzaifa-Ayub <m.huzaifa.ayub.510@gmail.com>
2026-09-08 20:13:45 +0500

    solve

--- 191.cpp
+++ 191.cpp
@@ -1,5 +1,6 @@
 /*
 191. Number of 1 Bits
+
 Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
 
 Example 1:
